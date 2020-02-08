511264db56a99450373af5bd5cd173171d5a88fc
keisuke.kato <kgulliverk@hotmail.com>
2020-02-08 15:56:17 +0900

    登録フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-# <%= form_with(model: @blog, local: true ,url: confirm_blogs_path ) do |form| %>
   resources :blogs do
     collection do
       post :confirm
     end
   end
+  rresources :users, only: [:new, :create]
 end
